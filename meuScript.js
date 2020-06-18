c7fd71e64915a35a0968e0e3f88f527d1e916481
PauloDev31 <mylifeispb@hotmail.com>
2020-06-17 22:42:01 -0300

    Primeiro commit

---
+++ meuScript.js
@@ -0,0 +1,9 @@
+let numero1 = parseInt(window.prompt("Insira um numero."));
+    alert ("Você confirma o número " + numero1+" ?");
+let numero2 = parseInt(window.prompt("Insira um numero."));
+    alert ("Você confirma o número " + numero2+" ?");
+if (numero1 > numero2) {
+    alert ("O número " +numero1+ " é maior");
+} else {
+    alert("O numero " +numero2+ " é maior");
+}
